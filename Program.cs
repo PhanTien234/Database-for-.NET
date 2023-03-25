741126fede2d8a7e16f74ac5709ae9b6270a5ea2
PhanTien234 <minhtienp328@gmail.com>
2023-03-25 21:20:10 +0700

    use SqlCOnnectionStringBUilder to create another way to set connect with SQL server

--- Program.cs
+++ Program.cs
@@ -5,7 +5,15 @@ using System.Data.SqlClient;
 namespace adodotnet{
     class Program{
         static void Main(string[] args){
-            string sqlStringConnection = "Data Source = localhost,1433;Initial Catalog=xtlab;User ID=sa;Password=Password123";
+            var sqlStringBuilder = new SqlConnectionStringBuilder();
+            sqlStringBuilder["Server"] = "localhost, 1433";
+            sqlStringBuilder["Database"] = "xtlab";
+            sqlStringBuilder["UID"] = "sa";
+            sqlStringBuilder["PWD"] = "Password123";
+
+            var sqlStringConnection = sqlStringBuilder.ToString();
+            Console.WriteLine(sqlStringConnection);
+            // string sqlStringConnection = "Data Source = localhost,1433;Initial Catalog=xtlab;User ID=sa;Password=Password123";
             using var connection = new SqlConnection(sqlStringConnection);
             connection.Open();
             Console.WriteLine(connection.State);
