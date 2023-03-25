f4dc54de7988d6c4c972e66d3cd4830ed1eb6bbc
PhanTien234 <minhtienp328@gmail.com>
2023-03-26 00:20:41 +0700

    connect database with MySQLStringBuilder

--- Program.cs
+++ Program.cs
@@ -1,27 +1,29 @@
 ﻿using System.Runtime.InteropServices;
 using System;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Data.Common;
 
 namespace adodotnet{
     class Program{
         static void Main(string[] args){
-            var sqlStringBuilder = new SqlConnectionStringBuilder();
-            sqlStringBuilder["Server"] = "localhost, 1433";
-            sqlStringBuilder["Database"] = "xtlab";
-            sqlStringBuilder["UID"] = "sa";
-            sqlStringBuilder["PWD"] = "Password123";
+            var sqlStringBuilder = new MySqlConnectionStringBuilder();  //SqlConnectionStringBuilder();
+            sqlStringBuilder["Server"] = "localhost";
+            sqlStringBuilder["Database"] = "new_schema";
+            sqlStringBuilder["UID"] = "root";
+            sqlStringBuilder["PWD"] = "abc123";
+            sqlStringBuilder["Port"] = "3307";
 
             var sqlStringConnection = sqlStringBuilder.ToString();
             Console.WriteLine(sqlStringConnection);
             // string sqlStringConnection = "Data Source = localhost,1433;Initial Catalog=xtlab;User ID=sa;Password=Password123";
-            using var connection = new SqlConnection(sqlStringConnection);
+            using var connection = new MySqlConnection(sqlStringConnection);//SqlConnection(sqlStringConnection);
             connection.Open();
             Console.WriteLine(connection.State);
              //..truy van
-            using DbCommand command = new SqlCommand();
+            using DbCommand command = new MySqlCommand();
             command.Connection = connection;
-            command.CommandText = "SELECT TOP (10) * FROM Sanpham";
+            command.CommandText = "SELECT * FROM Sanpham Limit 0,10";
 
             var datareader = command.ExecuteReader();
             while (datareader.Read())
