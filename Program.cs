6bb3151aa620f79364b1b64eef5ce38bb8e07430
PhanTien234 <minhtienp328@gmail.com>
2023-03-25 21:09:37 +0700

    use string to connect SQL server

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Data.SqlClient;
+
+namespace adodotnet{
+    class Program{
+        static void Main(string[] args){
+            string sqlStringConnection = "Data Source = localhost,1433;Initial Catalog=xtlab;User ID=sa;Password=Password123";
+            using var connection = new SqlConnection(sqlStringConnection);
+            connection.Open();
+            Console.WriteLine(connection.State);
+             //..truy van
+            connection.Close();
+        }
+    }
+}
